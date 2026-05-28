fcbb96619b3ae7ac7cdcd4708e33c54c07e472a2
alessandropetreti <petretiale@gmail.com>
2026-05-28 14:43:27 +0200

    Reso immutabile Accumulator, Aggiunta interfaccia FSStatLib, aggiunto metodo getFSReport

--- src/main/java/Accumulator.java
+++ src/main/java/Accumulator.java
@@ -1,19 +1,33 @@
+import java.util.Arrays;
+
 public class Accumulator {
-    private int totalFiles = 0;
+
+    private final int totalFiles;
     private final long[] bands;
     private final long maxFS;
     private final int nb;
 
+
+//    public static Accumulator of(long maxFS, int nb) {
+//        return new Accumulator(new long[nb + 1], maxFS, nb, 0);
+//    }
+
     public Accumulator(long maxFS, int nb) {
-        this.maxFS = maxFS;
-        this.nb = nb;
-        this.bands = new long[nb + 1];
+        this(new long[nb + 1], maxFS, nb, 0);
     }
 
-    public void addFile(long size) {
-        totalFiles++;
+    private Accumulator(long[] bands, long maxFS, int nb, int totalFiles) {
+        this.bands = bands;
+        this.maxFS = maxFS;
+        this.nb = nb;
+        this.totalFiles = totalFiles;
+    }
+
+    public Accumulator addFile(long size) {
+        final var newBands = Arrays.copyOf(bands, bands.length);
         if (size >= maxFS) {
-            bands[nb]++;
+            newBands[nb]++;
+            return new Accumulator(newBands, maxFS, nb, totalFiles + 1);
         } else {
             double width = (double) maxFS / nb;
             int index = (int) (size / width);
@@ -21,8 +35,21 @@ public class Accumulator {
             if (index >= nb) {
                 index = nb - 1;
             }
-            bands[index]++;
+            newBands[index]++;
+            return new Accumulator(newBands, maxFS, nb, totalFiles + 1);
         }
+    }
+
+    public Accumulator add(Accumulator acc) {
+        final var newBands = Arrays.copyOf(bands, bands.length);
+        for (int i = 0; i < bands.length; i++) {
+            newBands[i] = newBands[i] + acc.bands[i];
+        }
+        return new Accumulator(newBands, maxFS, nb, this.totalFiles + acc.totalFiles);
+    }
+
+    public int getTotalFiles() {
+        return totalFiles;
     }
 
     public void printStats() {
